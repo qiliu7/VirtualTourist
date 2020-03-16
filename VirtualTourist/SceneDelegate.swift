6ac8407347f827425f39e4fcc92726499f89985c
Qi <qil244@mail.usask.ca>
2020-03-15 22:35:30 -0600

    Add context save when necessary

--- VirtualTourist/SceneDelegate.swift
+++ VirtualTourist/SceneDelegate.swift
@@ -29,6 +29,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // This occurs shortly after the scene enters the background, or when its session is discarded.
     // Release any resources associated with this scene that can be re-created the next time the scene connects.
     // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    dataController.saveContext()
   }
 
   func sceneDidBecomeActive(_ scene: UIScene) {
@@ -39,6 +40,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func sceneWillResignActive(_ scene: UIScene) {
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
+    dataController.saveContext()
   }
 
   func sceneWillEnterForeground(_ scene: UIScene) {
@@ -52,7 +54,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // to restore the scene back to its current state.
 
     // Save changes in the application's managed object context when the application transitions to the background.
-    (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    dataController.saveContext()
   }
 
 

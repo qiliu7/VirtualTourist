7391d919a5966cf28446d6957200ba8eb8ce8bac
Qi <qil244@mail.usask.ca>
2020-03-15 22:32:59 -0600

    Move context save to DataController class

--- VirtualTourist/Model/DataController.swift
+++ VirtualTourist/Model/DataController.swift
@@ -11,23 +11,37 @@ import CoreData
 
 class DataController {
   
-  let persistenContainer: NSPersistentContainer
+  let persistentContainer: NSPersistentContainer
   
   var viewContext: NSManagedObjectContext {
-    return persistenContainer.viewContext
+    return persistentContainer.viewContext
   }
   
   init(modelName: String) {
-    persistenContainer = NSPersistentContainer(name: modelName)
+    persistentContainer = NSPersistentContainer(name: modelName)
   }
   
   func load(completion: (() -> Void)? = nil) {
-    persistenContainer.loadPersistentStores { (storeDescription, error) in
+    persistentContainer.loadPersistentStores { (storeDescription, error) in
       guard error == nil else {
         fatalError(error!.localizedDescription)
       }
       completion?()
     }
   }
+
+   func saveContext () {
+       let context = persistentContainer.viewContext
+       if context.hasChanges {
+           do {
+               try context.save()
+           } catch {
+               // Replace this implementation with code to handle the error appropriately.
+               // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+               let nserror = error as NSError
+               fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+           }
+       }
+   }
   
 }
